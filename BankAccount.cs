9365b01e2d07699003200f3a0b0272328a32057a
ahmedhh33 <ahmedhh3633@gmail.com>
2023-06-13 22:21:31 +0400

    Bank account clas

---
+++ BankAccount.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace OOPBsicsTasks
+{
+    internal class BankAccount
+    {
+        public string AccountNumber;
+        public string OwnertName;
+        public double Balance;
+        public double Amount;
+
+        public BankAccount(string accountNumber, string ownertName,double balance)
+        {
+            this.AccountNumber = accountNumber;
+            this.OwnertName = ownertName;
+            this.Balance = balance;
+            Amount = Disposit(50);
+        }
+
+        public double Disposit(double amount)
+        {
+            return  Balance +amount;
+        }
+
+        public double Withdraw(double amount ) 
+        {
+            if (amount> Balance)
+            {
+                return 0;
+            }
+            //Balance = Balance -amount;
+            return Balance - amount; 
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -27,6 +27,17 @@
             Circle circle2 = new Circle(5);// EX 2
             Console.WriteLine("The area of " + "circle2" + " = " + circle2.Area);
 
+            Console.WriteLine();
+            Console.WriteLine();
+
+            BankAccount myAccount = new BankAccount("92544","Ahmed Humaid",100);
+            //myAccount.Disposit(50);
+            Console.WriteLine(myAccount.Disposit(50));
+            //myAccount.Withdraw(75);
+            Console.WriteLine(myAccount.Withdraw(75));
+            //myAccount.Withdraw(100);
+            Console.WriteLine(myAccount.Withdraw(100));
+
         }
     }
 }
